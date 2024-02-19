b151ff6bd36280c7d5bb01bee782b87f7d5a0346
JacobWeisenburger <jacob.weisenburger@gmail.com>
2024-02-18 19:34:39 -0600

    changes

--- dist/index.js
+++ dist/index.js
@@ -1,28 +1,4 @@
-var __defProp = Object.defineProperty;
-var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, {
-      get: all[name],
-      enumerable: true,
-      configurable: true,
-      set: (newValue) => all[name] = () => newValue
-    });
-};
+var p=Object.defineProperty;var t=(f,e)=>{for(var o in e)p(f,o,{get:e[o],enumerable:!0,configurable:!0,set:(m)=>e[o]=()=>m})};var r={};t(r,{foo:()=>{{return x}}});function x(){return"foo"}export{r as fooModule};
 
-// src/foo.tsule.ts
-var exports_fooModule = {};
-__export(exports_fooModule, {
-  foo: () => {
-    {
-      return foo;
-    }
-  }
-});
-
-// src/foo.ts
-function foo() {
-  return "foo";
-}
-export {
-  exports_fooModule as fooModule
-};
+//# debugId=012E117FADB9841964756e2164756e21
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiLi4vc3JjL2Zvby50cyJdLAogICJzb3VyY2VzQ29udGVudCI6IFsKICAgICJleHBvcnQgZnVuY3Rpb24gZm9vICgpIHsgcmV0dXJuICdmb28nIH0iCiAgXSwKICAibWFwcGluZ3MiOiAiQUFBTyxTQUFTLENBQUksRUFBRyxDQUFFLE1BQU8iLAogICJkZWJ1Z0lkIjogIjAxMkUxMTdGQURCOTg0MTk2NDc1NmUyMTY0NzU2ZTIxIiwKICAibmFtZXMiOiBbXQp9
--- scripts/build.ts
+++ scripts/build.ts
@@ -1,4 +1,5 @@
-import dts from 'bun-plugin-dts'
+import { BunPlugin } from 'bun'
+import { EntryPointConfig, generateDtsBundle } from 'dts-bundle-generator'
 import { rm } from 'node:fs/promises'
 import * as Path from 'node:path'
 
@@ -13,9 +14,35 @@ console.log( 'dist: deleted' )
 await Bun.build( {
     entrypoints: [ './src/index.ts' ],
     outdir: './dist',
+    format: 'esm',
+    minify: true,
+    sourcemap: 'inline',
+    target: 'node',
     plugins: [
-        dts()
+        dtsPlugin()
     ],
 } )
 
 console.log( 'Build: done' )
+
+function dtsPlugin (): BunPlugin {
+    const output: EntryPointConfig[ 'output' ] = { noBanner: true }
+    return {
+        name: 'my-plugin',
+        target: 'node',
+        setup ( build ) {
+            const outDir = build.config.outdir || './dist'
+
+            build.config.entrypoints
+                .map( filePath => ( { filePath, output } ) )
+                .forEach( config => {
+                    const [ output ] = generateDtsBundle( [ config ] )
+                    const dtsFileName = config.filePath
+                        .replace( /^.*\//, '' )
+                        .replace( /\.[jtm]s$/, '.d.ts' )
+                    const outFile = Path.join( outDir, dtsFileName )
+                    return Bun.write( outFile, output )
+                } )
+        },
+    }
+}
